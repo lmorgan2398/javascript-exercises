54e044b021a59a31518eb94248a2078be42605ac
lmorgan2398 <lmorgan2398@gmail.com>
2024-06-24 11:22:11 -0500

    Create function repeatString and enable all tests
    This function accepts a string and a number as arguments, repeating
    the string the given number of times and returning a new string. All
    tests were passed by the given function, but code was less succinct
    than the given solution.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString;
+    if (num === 0) {
+        newString = '';
+    } else if (num < 0) {
+        newString = 'ERROR';
+    } else {
+        newString = string;
+        for (i = 1; i < (Math.floor(num)); i++) {
+            newString += string;
+        }
+    }
+    return newString;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
