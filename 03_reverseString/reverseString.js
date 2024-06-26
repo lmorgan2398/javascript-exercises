74c50d3d50946dc9449a2ef5c0c782f490bb5a77
lmorgan2398 <lmorgan2398@gmail.com>
2024-06-26 11:19:18 -0500

    Create and test function reverseString
    This function takes a given string, and locates the character
    at each position from the end of the string backwards, returning
    the same string but reversed. All tests passed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    let length = string.length;
+    for ((i = (length - 1)); i >= 0; i--) {
+        reversedString += string.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
