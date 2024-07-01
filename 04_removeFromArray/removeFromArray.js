8711c9319f48d0b91ab044e38da13a48ad5b8937
lmorgan2398 <lmorgan2398@gmail.com>
2024-06-30 22:56:48 -0500

    Create and test function to remove selected items from array
    Created a function over the course of several days that
    took an array and several option arguments to be removed from
    said array. Through nested looping and learning about rest
    parameters and redefining the "arguments" object, completed the
    task. Was stuck for a while on making a copy of an array to
    return while maintaining an unchanged array for the sake of the
    function. This one stumped me for a while.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(...args) {
+    let arr = Array.from(args);
+    let stringInput = arr[0];
+    let stringOutput = stringInput.slice();
+    let toRemove = arr.slice(1);
+    for (i = 0; i < stringInput.length; i++) {
+        for (j = 0; j < toRemove.length; j++) {
+            if (stringInput[i] === toRemove[j]) { 
+                stringOutput.splice(stringOutput.indexOf(stringInput[i]), 1); 
+                }
+            }
+        }
+    return stringOutput
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
