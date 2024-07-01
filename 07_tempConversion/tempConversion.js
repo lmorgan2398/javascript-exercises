31e10f4e335c7e93ea18c1df664529ba82a61442
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-01 12:42:41 -0500

    Create and test two temperature conversion functions
    Created functions convertToCelsius and convertToFarenheit
    to convert temperatures to the indicated format. For readability,
    the returned numbers are rounded to the nearest tenth.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitToConvert) {
+  let celsiusConverted;
+  celsiusConverted = ((farenheitToConvert - 32) * (5 / 9));
+  let celsiusConvertedRounded = +(celsiusConverted.toFixed(1));
+  return celsiusConvertedRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusToConvert) {
+  let farenheitConverted;
+  farenheitConverted = (celsiusToConvert * (9 / 5) + 32);
+  let farenheitConvertedRounded = +(farenheitConverted.toFixed(1));
+  return farenheitConvertedRounded;
 };
 
 // Do not edit below this line
