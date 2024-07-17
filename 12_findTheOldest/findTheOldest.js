47bb6c6b04f4a653c1078694f853772146eb79cf
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-16 23:48:27 -0500

    Create and test function findTheOldest
    Create a function that sifts through array objects
    and reorders them by age. The first (oldest) object
    is returned in its entirety.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.forEach((item) => { 
+        if (item.yearOfDeath) {
+            item.age = (item.yearOfDeath - item.yearOfBirth);
+        } else {
+            let currentTime = new Date();
+            let year = currentTime.getFullYear();
+            item.age = (year - item.yearOfBirth);
+        }
+    });
+    let arrByAge = arr.sort((a, b) => {
+        if (a.age === b.age) {
+            return 0;
+        } else if (a.age > b.age) {
+            return -1;
+        } else if (a.age < b.age) {
+            return 1;
+        }
+    });
+    return arrByAge[0];
 };
 
 // Do not edit below this line
