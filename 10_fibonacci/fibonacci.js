cd2cecf8d4a83d9acd5e7380826cb2390ea4d2da
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-16 20:49:23 -0500

    Create and test fibonacci function
    Created a function that loops through the
    Fibonacci sequence and gives you the number
    at the location specified in the arguments

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let lastNum = 0;
+    let currentNum = 1;
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            let newNum = currentNum + lastNum;
+            lastNum = currentNum;
+            currentNum = newNum;
+        }
+    } else {lastNum = 'OOPS'}
+    return lastNum;
 };
 
 // Do not edit below this line
