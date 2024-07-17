168de65ca41ce34b12592ae781792067fb9d1921
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-16 19:04:52 -0500

    Create and test calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,43 @@
-const add = function() {
-	
+const add = function(...args) {
+  let arr = [...args];
+  let sum = arr.reduce((total, currentNumber) => {
+              return total + currentNumber
+              }, 0);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let summedNumbers = arr.reduce((total, currentNumber) => {
+              return total + currentNumber
+              }, 0);
+  return summedNumbers;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = arr.reduce((total, currentNumber) => {
+              return total * currentNumber
+              });
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let sum = num;
+  if (num === 0) {
+    return 1;
+  } else {
+  for (i = num - 1; i > 0; i--) {
+    sum *= i;
+  };
+  return sum;
+};
 	
 };
 
