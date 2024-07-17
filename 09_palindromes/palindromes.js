0e4aa3cff71c08aef6e92a2f22603a4fa16bc2be
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-16 19:45:30 -0500

    Code and test palindrome function
    Created a function that tests whether a string is
    a palindrome or not by converting the string into
    an array of each character in the string, filtering
    out punctuation and spacing, the rejoining the array
    into a string. The string is then tested against itself
    flipped to determine if it is a palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strCharacters = Array.from(str.split(''));
+    let strLetters = strCharacters.filter((character) => character !== ' ' && 
+        character !== '.' &&
+        character !== ',' &&
+        character !== '!' &&
+        character !== '?' );
+    let strLettersLowerCase = strLetters.map((letter) => letter.toLowerCase())
+    let strLettersLowerCaseReversed = strLettersLowerCase.slice().reverse();
+    if (strLettersLowerCase.join() === strLettersLowerCaseReversed.join()) {
+        return true;
+    } else { return false;}
 };
 
 // Do not edit below this line
